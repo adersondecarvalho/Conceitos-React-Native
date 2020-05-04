6e633b8d8cedd15f5d4d7b0be995d381cb9923af
Aderson de Carvalho <acarvalho@inf.ufsm.br>
2020-05-04 19:37:49 -0300

    Remove error App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ export default function App() {
   async function handleLikeRepository(id) {
     const response = await api.post(`repositories/${id}/like`);
 
-    if (response.status === 200 )
+    if (response.status === 200 ) {
       const repositoryIndex = repositories.findIndex(repository => repository.id === id);
       const newRepositories = repositories.slice();
       newRepositories[repositoryIndex].likes ++;
